86ed4c558bcd233589885fba01d463ca9297ecd5
i-am-chirantan <acharyya.chirantan@gmail.com>
2022-02-09 19:37:37 +0530

    QR Code Scanner Added and write in qr modified

--- Write_in_qr.py
+++ Write_in_qr.py
@@ -5,6 +5,7 @@ wheel_number=input("Enter the wheel number: ")
 registration_number=input("Enter the registration number: ")
 model_number=input("Enter the model number: ")
 average_weight=input("Enter the average weight: ")
+average_weight=float(average_weight)
 
 
 data = {"registration_number":registration_number,"model_number":model_number,"average_weight":average_weight,"wheel_number":wheel_number}
