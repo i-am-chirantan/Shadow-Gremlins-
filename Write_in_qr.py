71a62d6b35a292f233aa472fae0c0ce36daf5cf8
i-am-chirantan <acharyya.chirantan@gmail.com>
2022-02-09 18:44:14 +0530

    Writing in qr updated

--- Write_in_qr.py
+++ Write_in_qr.py
@@ -1,10 +1,13 @@
 import qrcode
 
-car_number=input("Enter the car number: ")
+#car_number=input("Enter the car number: ")
+wheel_number=input("Enter the wheel number: ")
+registration_number=input("Enter the registration number: ")
 model_number=input("Enter the model number: ")
 average_weight=input("Enter the average weight: ")
 
-data = {"car_number":car_number, "model_number":model_number, "average_weight":average_weight}
+
+data = {"registration_number":registration_number,"model_number":model_number,"average_weight":average_weight,"wheel_number":wheel_number}
 
 
 img = qrcode.make(data)
